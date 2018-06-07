5d6c9813a6d533fd4c1757a3b378822bb810ba71
BackToShadow <li.yir@husky.neu.edu>
2018-06-06 23:54:05 -0400

    v3

---
+++ Assets/Script/PlayerHealth.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public static int curHealth;
+
+    private void Start()
+    {
+        curHealth = 5;
+    }
+
+}
---
+++ Assets/Script/WinDetect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinDetect : MonoBehaviour {
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            GameSceneManager.win = true;
+        }
+    }
+}

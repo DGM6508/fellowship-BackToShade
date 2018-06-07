fb0e60980b2468a05b407afe3817c366a4fb8934
BackToShadow <li.yir@husky.neu.edu>
2018-06-06 23:44:31 -0400

    v1

---
+++ Assets/Script/GameSceneManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager : MonoBehaviour {
+
+    public GameObject GameOverTxt;
+    public GameObject GameWinTxt;
+    public GameObject RestartButton;
+    public GameObject StartButton;
+    public GameObject MainMenu;
+    public static bool isGameStarted = false;
+    public GameObject SpikeGroup;
+    public GameObject Anchor;
+    public GameObject player;
+    public static bool win = false;
+
+    // Use this for initialization
+    void Start () {
+        GameOverTxt.SetActive(false);
+        GameWinTxt.SetActive(false);
+        RestartButton.SetActive(false);
+        isGameStarted = false;
+        SpikeGroup.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameHasEnded();
+        GameWin();
+        if (player.transform.position.x > Anchor.transform.position.x)
+        {
+            SpikeGroup.SetActive(true);
+        }
+	}
+
+    void GameHasEnded()
+    {
+        if (Player1MoveManager.curHealth == 0)
+        {
+            GameOverTxt.SetActive(true);
+            RestartButton.SetActive(true);
+        }
+    }
+
+    public void OnClickRestart ()
+    {
+        SceneManager.LoadScene("Scene01");
+        Player1MoveManager.stopIt = false;
+        Player1MoveManager.curHealth = 5;
+        win = false;
+        isGameStarted = false;
+    }
+
+    public void OnClickStart ()
+    {
+        isGameStarted = true;
+        MainMenu.SetActive(false);
+    }
+
+    void GameWin()
+    {
+        if (win)
+        {
+            GameWinTxt.SetActive(true);
+            RestartButton.SetActive(true);
+            print("123");
+        }
+
+    }
+}
---
+++ Assets/Script/Water.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour {
+
+    public float moveSpeed;
+
+    void Update ()
+    {
+        if (GameSceneManager.isGameStarted == true)
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, 0f);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            Player1MoveManager.curHealth = 0;
+            print(Player1MoveManager.curHealth);
+        }
+    }
+}
